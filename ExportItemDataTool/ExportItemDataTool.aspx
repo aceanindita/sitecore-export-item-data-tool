60b92fb1bf0f1b5b76cdd1f39f4adb4d76bbf7bc
Anindita Bhattacharya <aceanindita@gmail.com>
2015-04-21 02:39:25 +0530

    Export Item Data Tool V1.0
    This tool will let you export sitecore item data to CSV / XML and JSON -
    based on an index search with the input values passed in, in the tool
    front end.

---
+++ ExportItemDataTool/ExportItemDataTool.aspx
@@ -0,0 +1,109 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"
+    Inherits="SharedSource.Verndale.ExportData.ExportItemDataTool" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Export Item Data</title>
+    <script type="text/javascript">
+        function ClearStatus() {
+            var labelObj = document.getElementById("<%= lblStatus.ClientID %>");
+            labelObj.innerHTML = "";
+            return true;
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server" style="background: white; font-family: sans-serif; font-size: 13px">
+        <table style="padding: 10px;">
+            <tr>
+                <td>
+                    <asp:Label ID="lblLanguage" runat="server" Text="Language:"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtLanguage" runat="server" Width="350px" Text="en"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblIndexName" runat="server" Text="Index:"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtIndexName" runat="server" Width="350px" Text="sitecore_web_index"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblLocation" runat="server" Text="Location (Guid):"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtLocation" runat="server" Width="350px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblTemplate" runat="server" Text="Template (Comma Separated List of Template IDs):"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtTemplate" runat="server" Width="350px" TextMode="MultiLine" Height="70px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblInclude" runat="server" Text="Include:"></asp:Label>
+                </td>
+                <td>
+                    <asp:CheckBoxList ID="cboxListStandardFields" runat="server">
+                        <asp:ListItem Value="0" Selected="true">Item Id</asp:ListItem>
+                        <asp:ListItem Value="1" Selected="true">Item Name</asp:ListItem>
+                        <asp:ListItem Value="2">Template Id</asp:ListItem>
+                        <asp:ListItem Value="3">Path</asp:ListItem>
+                    </asp:CheckBoxList>
+                </td>
+            </tr>
+            <tr>
+                <td style="padding-right: 10px;">
+                    <asp:Label ID="lblFields1" runat="server" Text="Field Names (Comma Separated):"></asp:Label>
+                    <br />
+                    <asp:Label ID="lblFields2" runat="server" Text="If you need the value from a reference field item, use nested brackets."></asp:Label>
+                    <br />
+                    <asp:Label ID="lblFields3" runat="server" Text="eg: if Article Type, Necessary Products & ProductType were droplinks / multilists, "></asp:Label>
+                    <br />
+                    <asp:Label ID="lblFields4" runat="server"
+                        Text="Headline,Article Type(Title),Necessary Products(Sku,ProductType(Title)),Skill Level(Title)"
+                        Style="font-style: italic; color: #0033CC"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox ID="txtFieldNames" runat="server" TextMode="MultiLine" Height="70px" Width="350px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="lblFileFormat" runat="server" Text="File Format:"></asp:Label>
+                </td>
+                <td>
+                    <asp:RadioButtonList ID="rbtnList" runat="server">
+                        <asp:ListItem Value="0" Selected="True">CSV</asp:ListItem>
+                        <asp:ListItem Value="1">XML</asp:ListItem>
+                        <asp:ListItem Value="2">Json</asp:ListItem>
+                    </asp:RadioButtonList>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: center">
+                    <asp:Button ID="btnExport" runat="server" Text="Export" OnClick="btnExport_Click" OnClientClick="return ClearStatus();" />
+                </td>
+            </tr>
+            <tr>
+                <td style="height: 15px;"></td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: center; color: red">
+                    <asp:Label runat="server" ID="lblStatus"></asp:Label>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ ExportItemDataTool/ExportItemDataTool.aspx.cs
@@ -0,0 +1,394 @@
+﻿using Newtonsoft.Json;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Linq.Utilities;
+using Sitecore.ContentSearch.SearchTypes;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Xml;
+
+namespace SharedSource.Verndale.ExportData
+{
+    public partial class ExportItemDataTool : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        { }
+
+        protected void btnExport_Click(object sender, EventArgs e)
+        {
+            var items = GetItems(txtIndexName.Text, txtLanguage.Text, txtTemplate.Text, txtLocation.Text);
+
+            if (items != null && items.Any())
+            {
+                bool includeItemId = cboxListStandardFields.Items.FindByText("Item Id") != null
+                                     && cboxListStandardFields.Items.FindByText("Item Id").Selected;
+                bool includeItemName = cboxListStandardFields.Items.FindByText("Item Name") != null
+                                       && cboxListStandardFields.Items.FindByText("Item Name").Selected;
+                bool includeTemplateId = cboxListStandardFields.Items.FindByText("Template Id") != null
+                                         && cboxListStandardFields.Items.FindByText("Template Id").Selected;
+                bool includePath = cboxListStandardFields.Items.FindByText("Path") != null
+                                   && cboxListStandardFields.Items.FindByText("Path").Selected;
+
+                string fileContents = string.Empty;
+
+                List<LevelField> levelFields = null;
+                if (!string.IsNullOrWhiteSpace(txtFieldNames.Text))
+                {
+                    levelFields = GetLevelFields(txtFieldNames.Text);
+                }
+
+                #region CSV
+
+                if (rbtnList.SelectedItem.Text == "CSV")
+                {
+                    const string separator = ",";
+                    if (includeItemId)
+                    {
+                        fileContents += "Item Id" + separator;
+                    }
+
+                    if (includeItemName)
+                    {
+                        fileContents += "Item Name" + separator;
+                    }
+
+                    if (includeTemplateId)
+                    {
+                        fileContents += "Template Id" + separator;
+                    }
+
+                    if (includePath)
+                    {
+                        fileContents += "Path" + separator;
+                    }
+
+                    if (!string.IsNullOrWhiteSpace(txtFieldNames.Text))
+                    {
+                        if (levelFields == null)
+                        {
+                            lblStatus.Text = "Please check the format of the field string.";
+                            return;
+                        }
+
+                        if (IsAdditionalLevelFieldsExists(levelFields))
+                        {
+                            lblStatus.Text = "CSV Format supports only 1 level of nesting due to CSV format constraints! Pick a different format!";
+                            return;
+                        }
+
+                        fileContents = levelFields.Aggregate(fileContents, (current, field) => current + (field.Field + separator));
+                    }
+
+                    fileContents += "\r\n";
+
+                    foreach (var item in items)
+                    {
+                        if (includeItemId)
+                        {
+                            fileContents += item.ID.Guid + separator;
+                        }
+
+                        if (includeItemName)
+                        {
+                            fileContents += item.Name + separator;
+                        }
+
+                        if (includeTemplateId)
+                        {
+                            fileContents += item.TemplateID.Guid + separator;
+                        }
+
+                        if (includePath)
+                        {
+                            fileContents += item.Paths.FullPath + separator;
+                        }
+
+                        if (!string.IsNullOrWhiteSpace(txtFieldNames.Text) && levelFields != null)
+                        {
+                            fileContents = levelFields.Aggregate(fileContents, (current, field) => current
+                                + ((item.Fields[field.Field] != null ? "\"" + HttpUtility.HtmlDecode(item.Fields[field.Field].Value.Replace("\"", "\"\"")) + "\"" : string.Empty)
+                                + separator));
+                        }
+                        fileContents += "\r\n";
+                    }
+
+                    Response.Clear();
+                    Response.Buffer = true;
+                    Response.AddHeader("content-disposition", "attachment;filename=ExportedData.csv");
+                    Response.Charset = "";
+                    Response.ContentType = "application/text";
+                    Response.Output.Write(fileContents);
+                    Response.Flush();
+                    Response.End();
+                }
+
+                #endregion CSV
+
+                #region XML
+
+                if (rbtnList.SelectedItem.Text == "XML")
+                {
+                    StringWriter sw = GetXml(items, includeItemId, includeItemName, includeTemplateId, includePath, levelFields);
+
+                    Response.Clear();
+                    Response.ContentType = "application/text";
+                    Response.AddHeader("Content-Disposition:", "attachment;filename=ExportedData.xml");
+                    Response.Output.Write(sw);
+                    Response.End();
+
+                    sw.Flush();
+                    sw.Close();
+                }
+
+                #endregion XML
+
+                #region JSON
+
+                if (rbtnList.SelectedItem.Text == "Json")
+                {
+                    StringWriter sw = GetXml(items, includeItemId, includeItemName, includeTemplateId, includePath, levelFields);
+
+                    XmlDocument doc = new XmlDocument();
+                    doc.LoadXml(sw.ToString());
+                    string jsonText = JsonConvert.SerializeXmlNode(doc.SelectSingleNode("/Items"));
+
+                    sw.Flush();
+                    sw.Close();
+
+                    Response.Clear();
+                    Response.ContentType = "application/text";
+                    Response.AddHeader("Content-Disposition:", "attachment;filename=ExportedData.json");
+                    Response.Output.Write(jsonText);
+                    Response.End();
+                }
+
+                #endregion
+            }
+            else
+            {
+                lblStatus.Text = "No search results found.";
+            }
+        }
+
+        private List<LevelField> GetLevelFields(string fieldStr)
+        {
+            List<LevelField> levelFields = new List<LevelField>();
+            Stack<char> brackets = new Stack<char>();
+            string currentField = "";
+            string currentSubField = "";
+
+            foreach (char c in fieldStr)
+            {
+                if (c == '(')
+                {
+                    if (brackets.Count > 0)
+                    {
+                        currentSubField += c;
+                    }
+                    brackets.Push(c);
+                    continue;
+                }
+
+                if (c == ')')
+                {
+                    brackets.Pop();
+                    if (brackets.Count > 0)
+                    {
+                        currentSubField += c;
+                    }
+                    continue;
+                }
+
+                if (c == ',' && brackets.Count == 0)
+                {
+                    levelFields.Add(new LevelField { Field = currentField.Trim(), SubFields = currentSubField.Trim() });
+                    currentField = currentSubField = string.Empty;
+                    continue;
+                }
+
+                if (brackets.Count > 0)
+                {
+                    currentSubField += c;
+                }
+                else
+                {
+                    currentField += c;
+                }
+            }
+            levelFields.Add(new LevelField { Field = currentField.Trim(), SubFields = currentSubField.Trim() });
+
+            return brackets.Count != 0 ? null : levelFields;
+        }
+
+        private bool IsAdditionalLevelFieldsExists(IEnumerable<LevelField> levelFields)
+        {
+            return levelFields.Any(levelField => levelField.SubFields.Contains('('));
+        }
+
+        private StringWriter GetXml(IEnumerable<Item> items, bool includeItemId, bool includeItemName,
+            bool includeTemplateId, bool IncludePath, List<LevelField> levelFields)
+        {
+            StringWriter sw = new StringWriter();
+            using (XmlWriter writer = XmlWriter.Create(sw, new XmlWriterSettings { Indent = true, Encoding = System.Text.Encoding.UTF8 }))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Items"); //<Items>
+
+                foreach (var item in items)
+                {
+                    writer.WriteStartElement("Item"); //<Item>
+                    if (includeItemId)
+                    {
+                        writer.WriteStartElement("ItemId");
+                        writer.WriteString(item.ID.Guid.ToString());
+                        writer.WriteEndElement(); //</ItemId>
+                    }
+
+                    if (includeItemName)
+                    {
+                        writer.WriteStartElement("ItemName");
+                        writer.WriteString(item.Name);
+                        writer.WriteEndElement(); //</ItemId>
+                    }
+
+                    if (includeTemplateId)
+                    {
+                        writer.WriteStartElement("TemplateId");
+                        writer.WriteString(item.TemplateID.Guid.ToString());
+                        writer.WriteEndElement(); //</TemplateId>
+                    }
+
+                    if (IncludePath)
+                    {
+                        writer.WriteStartElement("Path");
+                        writer.WriteString(item.Paths.FullPath);
+                        writer.WriteEndElement(); //</Path>
+                    }
+
+                    if (levelFields != null)
+                    {
+                        foreach (var levelField in levelFields)
+                        {
+                            if (!string.IsNullOrWhiteSpace(levelField.Field))
+                            {
+                                if (!string.IsNullOrWhiteSpace(levelField.SubFields)
+                                    && item.Fields[levelField.Field] != null
+                                    && !string.IsNullOrWhiteSpace(item.Fields[levelField.Field].Value))
+                                {
+                                    foreach (var refItemId in item.Fields[levelField.Field].Value.Split('|').Where(x => !string.IsNullOrWhiteSpace(x)))
+                                    {
+                                        writer.WriteStartElement(levelField.Field.ToAlphanumeric());
+                                        Guid tempGuid;
+                                        if (Guid.TryParse(refItemId, out tempGuid))
+                                        {
+                                            Item tempItem = Sitecore.Context.Database.GetItem(Sitecore.Data.ID.Parse(tempGuid));
+                                            if (tempItem != null)
+                                            {
+                                                RecursiveFieldXMLWrite(writer, tempItem, levelField.SubFields);
+                                            }
+                                        }
+                                        writer.WriteEndElement(); //</<field>>
+                                    }
+                                }
+                                else if (item.Fields[levelField.Field] != null)
+                                {
+                                    writer.WriteStartElement(levelField.Field.ToAlphanumeric());
+                                    writer.WriteString(item.Fields[levelField.Field].Value);
+                                    writer.WriteEndElement(); //</<field>>
+                                }
+                            }
+                        }
+                    }
+                    writer.WriteEndElement(); //</Item>
+                }
+
+                writer.WriteEndElement(); //</Items>
+
+                writer.WriteEndDocument();
+                writer.Close();
+            }
+            return sw;
+        }
+
+        private void RecursiveFieldXMLWrite(XmlWriter writer, Item item, string fieldStr)
+        {
+            if (string.IsNullOrWhiteSpace(fieldStr)) return;
+
+            List<LevelField> currentLevelFields = GetLevelFields(fieldStr);
+            if (currentLevelFields != null && currentLevelFields.Any())
+            {
+                foreach (LevelField levelField in currentLevelFields)
+                {
+                    if (!string.IsNullOrWhiteSpace(levelField.Field))
+                    {
+                        if (!string.IsNullOrWhiteSpace(levelField.SubFields)
+                            && item.Fields[levelField.Field] != null
+                            && !string.IsNullOrWhiteSpace(item.Fields[levelField.Field].Value))
+                        {
+                            foreach (var refItemId in item.Fields[levelField.Field].Value.Split('|').Where(x => !string.IsNullOrWhiteSpace(x)))
+                            {
+                                writer.WriteStartElement(levelField.Field.ToAlphanumeric());
+                                Guid tempGuid;
+                                if (Guid.TryParse(refItemId, out tempGuid))
+                                {
+                                    Item tempItem = Sitecore.Context.Database.GetItem(Sitecore.Data.ID.Parse(tempGuid));
+                                    if (tempItem != null)
+                                    {
+                                        RecursiveFieldXMLWrite(writer, tempItem, levelField.SubFields);
+                                    }
+                                }
+                                writer.WriteEndElement(); //</<field>>
+                            }
+                        }
+                        else if (item.Fields[levelField.Field] != null)
+                        {
+                            writer.WriteStartElement(levelField.Field.ToAlphanumeric());
+                            writer.WriteString(item.Fields[levelField.Field].Value);
+                            writer.WriteEndElement(); //</<field>>
+                        }
+                    }
+                }
+            }
+        }
+
+        public static List<Item> GetItems(string indexName, string language, string templateIds, string locationId)
+        {
+            using (var context = ContentSearchManager.GetIndex(indexName).CreateSearchContext())
+            {
+                var query = context.GetQueryable<SearchResultItem>().Where(x => x.Language == language);
+                query = query.Where(x => x.Paths.Contains(Sitecore.Data.ID.Parse(locationId)));
+
+                var templateSearch = PredicateBuilder.True<SearchResultItem>();
+                if (!string.IsNullOrEmpty(templateIds))
+                {
+                    templateSearch = templateIds.Split(',').Where(x => !string.IsNullOrWhiteSpace(x)).Select(Sitecore.Data.ID.Parse).
+                        Aggregate(templateSearch, (current, newTemplateGuid) => current.Or(t => t.TemplateId == newTemplateGuid));
+                }
+
+                query = query.Where(templateSearch);
+                var count = query.Count();
+                return query.Take(count).Select(x => x.GetItem()).ToList();
+            }
+        }
+    }
+
+    public static class Extensions
+    {
+        public static string ToAlphanumeric(this string param)
+        {
+            return (new Regex("[^a-zA-Z0-9]")).Replace(param, "");
+        }
+    }
+
+    public class LevelField
+    {
+        public string Field { get; set; }
+        public string SubFields { get; set; }
+    }
+}
